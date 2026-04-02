dd6654e6d0763fb7fb01203ad5229172e7fcd9c4
SAMUEL B DE MOURA <samuelbdemoura@gmail.com>
2026-04-01 22:51:16 -0300

    Rename conversao de segundos.cpp to exercicio 2 -conversao de segundos.cpp

--- exercicios/exercicio 2 -conversao de segundos.cpp
+++ exercicios/exercicio 2 -conversao de segundos.cpp
@@ -8,6 +8,6 @@ int main(){
 	scanf("%d",&segundos);
 	printf("%d segundos sao: %.0d horas %.0d minutos %.0d segundos",segundos,segundos/3600,(segundos%3600)/60,(segundos%3600)%60);
 	/*meu raciocinio:1 hora sao 3600 segundos,entao dividi os segundos por isso e peguei a parte inteira
-	peguei o resto da divisao e dividi por 60 para decobrir os minutos,o resto dessa divisao ser· os segundos*/
+	peguei o resto da divisao e dividi por 60 para decobrir os minutos,o resto dessa divisao ser√° os segundos*/
 	return 0;
 }
