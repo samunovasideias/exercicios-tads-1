ef22cf2b4b960202517fb265ba7d19ffb34a7626
SAMUEL B DE MOURA <samuelbdemoura@gmail.com>
2026-04-02 14:41:31 -0300

    Refactor fuel consumption calculation program

--- exercicios/exercicio 1_consumo medio.cpp
+++ exercicios/exercicio 1_consumo medio.cpp
@@ -3,21 +3,73 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+ 
+#define erro 3	//constantes para codigo de erro ou se esta tudo ok
+#define ok 4
 
+int validar(int vali,int num);
 
-void limpar_buffer(void);
+int calculo_media(int distancia, int litros);
 
-int calculo_media(void);
+float gasto(int litros, float preco);
 
 int calculo_distancia(int media,int litros);
 
 int calculo_litros(int media, int distancia);
 
+int vali;//varievel global
+
 int main(){
 	
-	int med,opcao,lit,dist,validacao;
-
-	med=calculo_media();
+	int litros,distancia,opcao,lit,dist,validacao;
+	float preco,med;
+	/* litros e distancia sao usados na main para media,
+	lit e dist para calculos apos isso,
+	dentro das funcoes usei litros e distancia normalmente*/
+	
+	printf("bem vindo ao programa de calculo de consumo de combustivel \n");
+	
+	do{
+	
+	printf("digite a distancia percorrida pelo carro:");
+	validacao=scanf("%d",&distancia);
+	vali=validar(validacao,distancia);
+	if(vali==erro){
+		printf("numero incalido,tente noovamente");
+	}
+	
+	}while(vali==erro);
+	
+	do{
+	
+		printf("digite quantos litros foram gastos:\n");
+		validacao=scanf("%d",&litros); //o scanf gera o numer de variaveis lidas quando consegue ler todas,se for menor que isso,nao leu
+		vali=validar(validacao,litros);
+		if(vali==erro){
+		printf("numero incalido,tente noovamente");
+		}
+		
+	}while(vali==erro);
+	
+	do{
+	
+		printf("digite o preço da gasolina //L:\n");
+		validacao=scanf("%f",&preco); //o scanf gera o numer de variaveis lidas quando consegue ler todas,se for menor que isso,nao leu
+		vali=validar(validacao,preco);
+		if(vali==erro){
+		printf("numero incalido,tente noovamente");
+		}
+		
+	}while(vali==erro);
+	
+	
+	med=calculo_media(distancia,litros);
+	
+	
+	printf("seu consumo medio e de %.2f km\\l \n", med);
+	
+	printf("seu gasto total sera: R$ %.2f\n",gasto(litros,preco));
+	
 	
 	do{
 		printf("deseja saber:\n1. quantos km fara com n litros \n2. quantos litros precisara para percorrer uma distancia\n");
@@ -36,31 +88,33 @@ int main(){
 				
 			printf("quantos litros?\n");
 			validacao=scanf("%d",&lit);
+			vali=validar(validacao,lit);
+	
+				}while(vali==erro);
 			
-			if(validacao!=1){   
-				
-				limpar_buffer();
-				
-				}
-				}while(validacao!=1);
+			printf("a distancia percorrida sera: %d\n",calculo_distancia(med,lit));
+			printf("seu gasto total sera %.2f\n",gasto(lit,preco));
 			
-			printf("a distancia percorrida sera: %d ",calculo_distancia(med,lit));
 		break;
 		
 		case 2:
 			do{
 				printf("qual a distancia a percorrer?\n");
 			validacao=scanf("%d",&dist);
+			vali=validar(validacao,dist);
 				
-				if(validacao!=1){   
+				}while(vali==erro);
 				
-				limpar_buffer();
-				
-				}
-				}while(validacao!=1);
+			lit=dist/med;
 			
 			printf("serao gastos: %d litros\n ",calculo_litros(med,dist));
+			printf("seu gasto total sera %.2f\n",gasto(lit,preco));
+			
 		break;
+		
+		default:
+			printf("nao foi possibel ler o numero digitado,programa finalizado");
+		return erro;	
 			
 	}
 	printf("programa finalizado");
@@ -69,45 +123,29 @@ int main(){
 }
 
 
-void limpar_buffer(void){
+int validar(int vali, int num){
 		int c;//para limpar buffer
-		while((c=getchar())!='\n' && c!=EOF);//limpa buffer,o getchar joga o buffer dentro da variavel c até encontrar um enter
+			if(vali!=1 || num<=0){   
+				while((c=getchar())!='\n' && c!=EOF);//limpa buffer,o getchar joga o buffer dentro da variavel c até encontrar um enter
+				return erro;
+		}
+		return ok;
 }
 
-int calculo_media(void){
-		int litros,distancia,validacao,media;
-	int c;//para limpar buffer
+int calculo_media(int distancia,int litros){
+		int media;
 	
-	printf("bem vindo ao programa de calculo de consumo de combustivel \n");
-	
-	do{
-	
-	printf("digite a distancia percorrida pelo carro:");
-	validacao=scanf("%d",&distancia);
-	
-	if(validacao!=1 || distancia<0){   
-		limpar_buffer();
-		}
-	
-	}while(validacao!=1 || distancia<0);
-	
-	do{
-	
-		printf("digite quantos litros foram gastos:\n");
-		validacao=scanf("%d",&litros); //o scanf gera o numer de variaveis lidas quando consegue ler todas,se for menor que isso,nao leu
-			
-			if(validacao!=1 || litros<0){   
-		printf("nao foi possivel ler o numero,digite novamente\n");
-		while((c=getchar())!='\n' && c!=EOF); 
-		}
-		
-	}while(validacao!=1 || litros<=0);
 	
 	media=distancia/litros; //calculo da media
 	
-	printf("seu consumo medio é de %d km\\l \n", media);
-	
 	return media;
+}
+
+float gasto(int litros, float preco){
+	float gastar;
+	gastar=litros*preco;
+	
+	return gastar;
 }
 
 int calculo_distancia(int media,int litros){
